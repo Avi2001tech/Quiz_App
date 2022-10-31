d6c3f65945a77a3f583a9bc634b7cda960d46c67
Abhishek Kumar Shaw <avitech2001@gmail.com>
2022-10-31 23:01:43 +0530

    Added new features along with firebase authentication

--- app/src/main/java/com/example/integration/profile_page.java
+++ app/src/main/java/com/example/integration/profile_page.java
@@ -7,27 +7,35 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.dhaval2404.imagepicker.ImagePicker;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.io.ByteArrayOutputStream;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class profile_page extends AppCompatActivity {
     Button sign_out;
     Button update;
+    LinearLayout display;
    // TextInputEditText display_name,display_email,display_phone_no,future_purpose_1,future_purpose_2,future_purpose_3;
     TextView user_name,user_small_name;
-    EditText display_n;
+    EditText display_n,display_email,display_phone_no,status;
     FloatingActionButton fab;
     CircleImageView imgUser;
     Dialog progressDialog;
@@ -42,9 +50,14 @@ public class profile_page extends AppCompatActivity {
         sign_out = findViewById(R.id.sign_out);
         update = findViewById(R.id.update);
 
+        display = findViewById(R.id.display);
+
         user_name = findViewById(R.id.email_login);
         user_small_name = findViewById(R.id.user_name_small);
         display_n = findViewById(R.id.display_name);
+        display_email = findViewById(R.id.display_email);
+        display_phone_no = findViewById(R.id.display_phone_no);
+        status = findViewById(R.id.status);
         imgUser = findViewById(R.id.user_pic);
 
         progressDialog = new Dialog(profile_page.this);
@@ -55,25 +68,18 @@ public class profile_page extends AppCompatActivity {
         dialog_text.setText("Signning out ....");
 
         mAuth = FirebaseAuth.getInstance();
-    //    display_name = findViewById(R.id.display_name);
-   //     display_email = findViewById(R.id.display_email);
-   //     display_phone_no = findViewById(R.id.display_phone_no);
+
+
    //     future_purpose_1 = findViewById(R.id.future_purpose_1);
    //     future_purpose_2 = findViewById(R.id.future_purpose_2);
    //     future_purpose_3 = findViewById(R.id.future_purpose_3);
 
-   //     String display_n = display_name.getText().toString();
-   //     String display_e = Objects.requireNonNull(display_email.getText()).toString();
-  //      String display_p = display_phone_no.getText().toString();
+  //
    //     String display_f1 = future_purpose_1.getText().toString();
    //     String display_f2 = future_purpose_2.getText().toString();
    //     String display_f3 = future_purpose_3.getText().toString();
 
-       // user_name.setText(display_n);
-       // user_small_name.setText(display_n);
-  //      display_name.setText(display_n);
- //       display_email.setText(display_e);
- //       display_phone_no.setText(display_p);
+
  //       future_purpose_1.setText(display_f1);
  //       future_purpose_2.setText(display_f2);
  //       future_purpose_3.setText(display_f3);
@@ -97,26 +103,51 @@ public class profile_page extends AppCompatActivity {
         update.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String s = display_n.getText().toString();
 
-                SharedPreferences pref = getSharedPreferences("display_name",MODE_PRIVATE);
-                SharedPreferences.Editor editor = pref.edit();
+                if(display.getVisibility() == View.GONE){
+                    display.setVisibility(View.VISIBLE);
+                    Toast.makeText(profile_page.this, "You can update your profile", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    String s = display_n.getText().toString();
+                    String display_e = display_email.getText().toString();
+                    String display_p = display_phone_no.getText().toString();
+                    String display_status = status.getText().toString();
 
-                editor.putString("str",s);
-                editor.apply();
 
-                display_n.setText(s);
-                user_name.setText(s);
-                user_small_name.setText(s);
-                Toast.makeText(profile_page.this, "Your profile is updated now", Toast.LENGTH_SHORT).show();
+                    SharedPreferences pref = getSharedPreferences("display",MODE_PRIVATE);
+                    SharedPreferences.Editor editor = pref.edit();
+
+                    editor.putString("str",s);
+                    editor.putString("email",display_e);
+                    editor.putString("phone_no",display_p);
+                    editor.putString("status",display_status);
+                    editor.apply();
+
+                    display_n.setText(s);
+                    user_name.setText(s);
+                    user_small_name.setText(display_status);
+                    display_email.setText(display_e);
+                    display_phone_no.setText(display_p);
+                    status.setText(display_status);
+                    Toast.makeText(profile_page.this, "Your profile is updated now", Toast.LENGTH_SHORT).show();
+                    display.setVisibility(View.GONE);
+                }
+
             }
         });
 
-        SharedPreferences getShared = getSharedPreferences("display_name",MODE_PRIVATE);
+        SharedPreferences getShared = getSharedPreferences("display",MODE_PRIVATE);
         String val = getShared.getString("str","User name");
+        String val2 = getShared.getString("email","sample_email@gmail.com");
+        String val3 = getShared.getString("phone_no","9876543210");
+        String val4 = getShared.getString("status","Your status will be displayed here");
         display_n.setText(val);
         user_name.setText(val);
-        user_small_name.setText(val);
+        user_small_name.setText(val4);
+        display_email.setText(val2);
+        display_phone_no.setText(val3);
+        status.setText(val4);
 
 
 
@@ -143,6 +174,7 @@ public class profile_page extends AppCompatActivity {
         if (resultCode == Activity.RESULT_OK) {
             Uri uri = data.getData();
             imgUser.setImageURI(uri);
+
             //Image Uri will not be null for RESULT_OK
 
         } else if (resultCode == ImagePicker.RESULT_ERROR) {
@@ -152,4 +184,5 @@ public class profile_page extends AppCompatActivity {
         }
 
     }
+
 }
